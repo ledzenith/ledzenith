c264e11b050c5db4ad3af2e9ea72d847b6aab655
Daniele-Biscari <biscari.daniele@gmail.com>
2023-10-21 13:56:06 +0200

    Deploy 1 try6

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <HashRouter>
         <Routes>
-          <Route exact path="/" element={<Home/>}/>
+          <Route path="/" element={<Home/>}/>
           <Route path="/soon" element={<WorkinProgress/>}/>
         </Routes>
     </HashRouter>
